bcd0d97e6b76bc46854f8e1f6b911a64c62c7bbc
Pablo Castillo <pablojoelcastilloflores@gmail.com>
2025-08-31 15:17:19 -0500

    REMOVE .vs/ and ADD .gitignore

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace REPOSITORIO_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hELLO WORLD");
+            Console.WriteLine("FINALLY");
+
+            Console.ReadKey();
+        }
+}
